ffb57eba0708955386abbae1e652a0d62053718f
NotCybot <141260979+NotCybot@users.noreply.github.com>
2024-12-04 19:38:49 +0100

    reworked extraction method

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::Error;
 use pdf_extract::extract_text;
 use rust_xlsxwriter::{cell_range, Format, Formula, Workbook};
-use std::{fs};
+use core::f64;
+use std::fs;
+use regex::Regex;
 
 fn main() -> Result<(), Error> {
     let pdf_dir = "pdf/";
@@ -48,8 +50,8 @@ fn main() -> Result<(), Error> {
     //* Row and column numbers are all zero-indexed.
     let first_row = 2; // Skip the header row.
     let last_row = first_row + (extracted_texts.len() as u32) - 1;
-    let netto_col = 15;
-    let brutto_col = 17;
+    let netto_col = 3;
+    let brutto_col = 5;
 
     let netto_range = cell_range(first_row, netto_col, last_row, netto_col);
     let netto_formula = format!("=SUM({})", netto_range);
@@ -62,55 +64,62 @@ fn main() -> Result<(), Error> {
     //* Write Headers
     worksheet.write_with_format(0, 0, "PDF File Name", &bold)?;
     worksheet.write_with_format(0, 1, "Belegnummer", &bold)?;
-    worksheet.write_with_format(0, 5, "Datum", &bold)?;
-    worksheet.write_with_format(0, 15, "Zwischensumme (Netto)", &bold)?;
-    worksheet.write_with_format(1, 15, Formula::new(netto_formula), &bold)?;
-    worksheet.write_with_format(0, 17, "Zwischensumme (Brutto)", &bold)?;
-    worksheet.write_with_format(1, 17, Formula::new(brutto_formula), &bold)?;
+    worksheet.write_with_format(0, 2, "Datum", &bold)?;
+    worksheet.write_with_format(0, 3, "Zwischensumme (Netto)", &bold)?;
+    worksheet.write_with_format(1, 3, Formula::new(netto_formula), &bold)?;
+    worksheet.write_with_format(0, 4, "USt", &bold)?;
+    worksheet.write_with_format(0, 5, "Zwischensumme (Brutto)", &bold)?;
+    worksheet.write_with_format(1, 5, Formula::new(brutto_formula), &bold)?;
 
     //* Set column width
     worksheet.set_column_width(0, 15)?;
     worksheet.set_column_width(1, 30)?;
-    worksheet.set_column_width(5, 25)?;
-    worksheet.set_column_width(15, 35)?;
-    worksheet.set_column_width(17, 35)?;
+    worksheet.set_column_width(2, 25)?;
+    worksheet.set_column_width(3, 35)?;
+    worksheet.set_column_width(4, 15)?;
+    worksheet.set_column_width(5, 35)?;
 
 //* Extract text from each PDF and store it in the vector
 let mut row = 2;
+let re = Regex::new(r"(\d+\.\d+)").unwrap();
 for (pdf_file, texts) in extracted_texts {
     worksheet.write(row, 0, &pdf_file)?;
-    for (col, text) in texts.iter().enumerate() {
-        let col_index = (col + 1) as u32;
-        if col_index == 15 || col_index == 17 {
-            // Debugging-Ausgabe
-            println!("Verarbeite Spalte {} in Zeile {} mit Wert: {}", col_index, row, text);
-    
-            // Entferne alle nicht-numerischen Zeichen außer Ziffern und Punkten
-            let mut value_str: String = text.chars().filter(|c| c.is_digit(10) || *c == '.').collect();
-            println!("Gefilterter Wert: {}", value_str); // Debugging-Ausgabe
-    
-            // Entferne vorangestellte Punkte
-            while value_str.starts_with('.') {
-                value_str.remove(0);
+
+    let mut receipt_number = String::new();
+    let mut date = String::new();
+    let mut netto_zwi = 0.0;
+    let mut brutto_zwi = 0.0;
+    let mut ust = String::new();
+
+
+    for text in texts {
+        if text.contains("Belegnummer") || text.contains("Receipt number") {
+            receipt_number = text.clone();
+        }
+        if text.contains("Datum") || text.contains("Date") {
+            date = text.clone();
+        }
+        if text.contains("Zwischensumme") || text.contains("Sub total") {
+            if let Some(caps) = re.captures(&text) {
+                netto_zwi = caps.get(0).map_or("NULL", |m| m.as_str()).parse::<f64>().unwrap_or(0.0);
             }
-            println!("Wert nach Entfernen der vorangestellten Punkte: {}", value_str); // Debugging-Ausgabe
-    
-            // Überprüfe, ob der String leer ist, und setze ihn ggf. auf "0"
-            if value_str.is_empty() {
-                value_str = "0".to_string();
+        }
+        if text.contains("USt. 0.00%") || text.contains("VAT 0.00%") {
+            ust = text.clone();
+        }
+        if text.contains("Gesamtbetrag") || text.contains("Total") {
+            if let Some(caps) = re.captures(&text) {
+                brutto_zwi = caps.get(0).map_or("NULL", |m| m.as_str()).parse::<f64>().unwrap_or(0.0);
             }
-    
-            // Konvertiere den bereinigten String in eine Zahl
-            let value = value_str.parse::<f64>().unwrap_or(0.0);
-            println!("Wert nach Parsen: {}", value); // Debugging-Ausgabe
-    
-            // Schreibe die Zahl in die Zelle
-            worksheet.write_number(row, col_index.try_into().unwrap(), value)?;
-        } else {
-            worksheet.write(row, col_index.try_into().unwrap(), text)?;
         }
     }
-    
+
+    worksheet.write(row, 1, &receipt_number)?;
+    worksheet.write(row, 2, &date)?;
+    worksheet.write(row, 3, netto_zwi)?;
+    worksheet.write(row, 4, &ust)?;
+    worksheet.write(row, 5, brutto_zwi)?;
+
     row += 1;
 }
 
